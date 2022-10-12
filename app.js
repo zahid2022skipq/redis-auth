4c8be1dc82dfc9aafa112370fde91b21920f5b86
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-10-12 18:03:23 +0500

    connect to redis server

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
+const { createClient } = require("redis");
+const connectRedis = require("connect-redis");
+
+const redisClient = createClient({ legacyMode: true });
+redisClient
+  .connect()
+  .catch((e) => console.log("cannot connect to the client" + e));
+const config = require("dotenv").config();
+
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
+
+const RedisStore = connectRedis(session);
+const sessionConfig = {
+  store: new RedisStore({ client: redisClient }),
+  resave: false,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    secure: false,
+    httpOnly: false,
+    maxAge: 1000 * 60 * 10,
+  },
+};
 
 const app = express();
 
@@ -13,9 +35,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sessionConfig));
 
 app.use("/", indexRouter);
 
 app.use("/auth", authRouter);
+
+app.use("/hello", (req, res) => {
+  if (req.session.viewCount === undefined) {
+    req.session.viewCount = 0;
+  } else {
+    req.session.viewCount++;
+  }
+  res.send("View Count is " + req.session.viewCount);
+});
 
 module.exports = app;
