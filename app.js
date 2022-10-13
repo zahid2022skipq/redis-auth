aabd200819f866c6adfa83349f4977d263e44228
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-10-13 18:49:39 +0500

    Setting up routes config

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sessionConfig));
 
 app.use("/", indexRouter);
@@ -49,5 +48,7 @@ app.use("/hello", (req, res) => {
   }
   res.send("View Count is " + req.session.viewCount);
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
